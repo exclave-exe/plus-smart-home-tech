0127d1a4bf969e1c239aab18dd7a84e6ac73c4ec
Nikita Ukhov <209745506+exclave-exe@users.noreply.github.com>
2026-05-17 20:34:01 +0500

    feat: fix enum

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/telemetry/analyzer/model/Operation.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/telemetry/analyzer/model/Operation.java
@@ -5,30 +5,22 @@ import ru.yandex.practicum.kafka.telemetry.event.ConditionOperationAvro;
 import java.util.function.BiFunction;
 
 public enum Operation {
-
-    EQUALS((l, r) -> l.compareTo(r) == 0),
-    GREATER_THAN((l, r) -> l.compareTo(r) > 0),
-    LOWER_THAN((l, r) -> l.compareTo(r) < 0);
-
-    private final BiFunction<Integer, Integer, Boolean> operation;
-
-    Operation(BiFunction<Integer, Integer, Boolean> operation) {
-        this.operation = operation;
-    }
+    EQUALS,
+    GREATER_THAN,
+    LOWER_THAN;
 
     public static Operation from(ConditionOperationAvro operation) {
-        for (Operation value : values()) {
-            if (value.name().equalsIgnoreCase(operation.name())) {
-                return value;
-            }
-        }
-        return null;
+        if (operation == null) return null;
+        return Operation.valueOf(operation.name());
     }
 
     public boolean apply(Integer left, Integer right) {
-        if (left == null || right == null) {
-            return false;
-        }
-        return operation.apply(left, right);
+        if (left == null || right == null) return false;
+
+        return switch (this) {
+            case EQUALS -> left.compareTo(right) == 0;
+            case GREATER_THAN -> left.compareTo(right) > 0;
+            case LOWER_THAN -> left.compareTo(right) < 0;
+        };
     }
 }
